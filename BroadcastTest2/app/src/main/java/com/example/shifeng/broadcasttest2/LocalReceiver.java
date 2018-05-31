9cd4fc0b9ccda933f21ddfe2c0f7e196b17d39d6
shifeng <shifeng199307@gmail.com>
2018-05-31 21:54:21 +0800

    tongbu

--- BroadcastTest2/app/src/main/java/com/example/shifeng/broadcasttest2/LocalReceiver.java
+++ BroadcastTest2/app/src/main/java/com/example/shifeng/broadcasttest2/LocalReceiver.java
@@ -5,10 +5,10 @@ import android.content.Context;
 import android.content.Intent;
 import android.widget.Toast;
 
-public class AnotherBroadcastReceiver extends BroadcastReceiver {
+public class LocalReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        Toast.makeText(context, "received in AnotherBroadcastReceiver", Toast.LENGTH_SHORT).show();
+        Toast.makeText(context,"hello local", Toast.LENGTH_SHORT).show();
     }
 }
