c63f4320d979f3c82a2fe85647649ef7f9aba4eb
shifeng <shifeng199307@gmail.com>
2018-06-15 09:53:05 +0800

    新增sqlite部分

--- DatabaseTest/app/src/main/java/com/example/shifeng/databasetest/MyDatabaseHelper.java
+++ DatabaseTest/app/src/main/java/com/example/shifeng/databasetest/MyDatabaseHelper.java
@@ -10,6 +10,8 @@ public class MyDatabaseHelper extends SQLiteOpenHelper {
 
     public static final String CREATE_BOOK = "create table Book (id integer primary key autoincrement, author text, price real, pages integer, name text)";
 
+    public static final String CREATE_CATEGORY = "create table Category (id integer primary key autoincrement, category_name text, category_code integer)";
+
     private Context mContext;
 
     public MyDatabaseHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
@@ -20,10 +22,14 @@ public class MyDatabaseHelper extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase db) {
         db.execSQL(CREATE_BOOK);
+        db.execSQL(CREATE_CATEGORY);
         Toast.makeText(mContext, "Create succeeded", Toast.LENGTH_SHORT).show();
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("drop table if exists Book");
+        db.execSQL("drop table if exists Category");
+        onCreate(db);
     }
 }
