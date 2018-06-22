0bbd44759849986ba241452b83435e1483e8ebb4
shifeng <shifeng199307@gmail.com>
2018-06-22 14:38:33 +0800

    sqlite

--- DatabaseTest/app/src/main/java/com/example/shifeng/databasetest/MainActivity.java
+++ DatabaseTest/app/src/main/java/com/example/shifeng/databasetest/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.shifeng.databasetest;
 
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -19,6 +23,77 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 dbHelper.getWritableDatabase();
+            }
+        });
+        // 新增
+        Button addData = (Button) findViewById(R.id.add_data);
+        addData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SQLiteDatabase db = dbHelper.getWritableDatabase();
+                ContentValues values = new ContentValues();
+                // 开始组装第一条数据
+                values.put("name", "The Da Vinci Code");
+                values.put("author", "Dan Brown");
+                values.put("pages", 454);
+                values.put("price", 16.96);
+                db.insert("Book", null, values); // 插入第一条数据
+                values.clear();
+                // 开始组装第二条数据
+                values.put("name", "The Lost Symbol");
+                values.put("author", "Dan Brown");
+                values.put("pages", 510);
+                values.put("price", 19.95);
+                db.insert("Book", null, values); // 插入第二条数据
+            }
+        });
+        // 查询
+        Button queryData = (Button) findViewById(R.id.query_data);
+        queryData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SQLiteDatabase db = dbHelper.getWritableDatabase();
+                // 查询 Book 表中所有的数据
+                Cursor cursor = db.query("Book", null, null, null, null, null, null);
+                if (cursor.moveToFirst()) {
+                    do {
+                        // 遍历 Cursor 对象，取出数据并打印
+                        String name = cursor.getString(cursor.getColumnIndex
+                                ("name"));
+                        String author = cursor.getString(cursor.getColumnIndex
+                                ("author"));
+                        int pages = cursor.getInt(cursor.getColumnIndex("pages"));
+                        double price = cursor.getDouble(cursor.getColumnIndex
+                                ("price"));
+                        Log.d("MainActivity", "book name is " + name);
+                        Log.d("MainActivity", "book author is " + author);
+                        Log.d("MainActivity", "book pages is " + pages);
+                        Log.d("MainActivity", "book price is " + price);
+                    } while (cursor.moveToNext());
+                }
+                cursor.close();
+            }
+        });
+        // 修改
+        Button updateData = (Button) findViewById(R.id.update_data);
+        updateData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SQLiteDatabase db = dbHelper.getWritableDatabase();
+                ContentValues values = new ContentValues();
+                values.put("price", 10.99);
+                String[] params = {"The Da Vinci Code"};   // 字符串数组
+                db.update("Book", values, "name = ?", params);
+            }
+        });
+        // 删除
+        Button deleteData = (Button) findViewById(R.id.delete_data);
+        deleteData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SQLiteDatabase db = dbHelper.getWritableDatabase();
+                String[] params = {"200"};
+                db.delete("Book", "pages > ?", params);
             }
         });
     }
